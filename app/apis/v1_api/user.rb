936013aee25762cf1d6c1eb53a16edfd6b33bb9b
seairy <seairy@gmail.com>
2015-01-27 19:17:23 +0800

    bug fixed

--- app/apis/v1_api/user.rb
+++ app/apis/v1_api/user.rb
@@ -2,9 +2,28 @@
 module V1API
   class User < Grape::API
     resources :users do
+      desc '简单用户注册'
+      post :sign_up_simple do
+        present uuid: SecureRandom.uuid
+      end
+
       desc '用户注册'
+      params do
+        requires :phone, type: String, desc: '手机号码'
+        requires :verification_code, type: String, desc: '验证码'
+      end
       post :sign_up do
         present uuid: SecureRandom.uuid
+      end
+    end
+
+    resources :verification_codes do
+      desc '获取验证码'
+      params do
+        requires :phone, type: String, desc: '手机号码'
+      end
+      post '/' do
+        present verification_code: rand(123456..987654)
       end
     end
   end
