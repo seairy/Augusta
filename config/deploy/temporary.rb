37a298461d55919a9e2bf657b6c00ade1405f4a3
seairy <seairy@gmail.com>
2015-01-27 15:22:38 +0800

    init commit

---
+++ config/deploy/temporary.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+set :stage, :temporary
+set :branch, 'master'
+set :deploy_user, 'seairy'
+
+server '60.247.115.253', port: 3030, user: 'root', roles: %w{web app db}, primary: true
+
+set :deploy_to, "/srv/www/Augusta"
+set :rails_env, :production
+
+set :config_files, %w(
+  abc
+)
