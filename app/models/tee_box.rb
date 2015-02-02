732673f6997e48e6d694dfb9315dc5535cf4c322
seairy <seairy@gmail.com>
2015-02-02 10:54:32 +0800

    init commit

--- app/models/tee_box.rb
+++ app/models/tee_box.rb
@@ -1,7 +1,7 @@
 class TeeBox < ActiveRecord::Base
   as_enum :color, [:red, :white, :blue, :black, :gold], prefix: true, map: :string
   belongs_to :hole
-  validates :distance_from_hole, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 999 }
+  validates :distance_from_hole, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 999 }
 
   class << self
     [:red, :white, :blue, :black, :gold].each do |color|
--- config.ru
+++ config.ru
@@ -1,3 +1,11 @@
+require 'rack/cors'
 require ::File.expand_path('../config/environment', __FILE__)
 
 run Rails.application
+
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+  end
+end
