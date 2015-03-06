0d7e8b1f38aa3a8cb8674f3850e4dfc5c83ac230
seairy <seairy@gmail.com>
2015-03-06 17:03:53 +0800

    init commit

--- app/models/scorecard.rb
+++ app/models/scorecard.rb
@@ -4,6 +4,7 @@ class Scorecard < ActiveRecord::Base
   as_enum :tee_box_color, [:red, :white, :blue, :black, :gold], prefix: true, map: :string
   belongs_to :match
   belongs_to :hole
+  default_scope -> { order(:number) }
   scope :sorted, -> { order(:number) }
   scope :out, -> { where(number: 1..9) }
   scope :in, -> { where(number: 10..18) }
