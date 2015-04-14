4d61d1506e2e6ab3f22ba8074689a4ae023233e8
seairy <seairy@gmail.com>
2015-04-14 20:19:23 +0800

    init commit

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -185,9 +185,10 @@ class Statistic < ActiveRecord::Base
         @up_and_downs_percentage = "#{@shots_within_100.zero? ? 0 : ((@up_and_downs_count.to_f / @shots_within_100) * 100).round(2)}%"
         @chip_ins = up_and_downs_scorecards.select{|scorecard| scorecard.chip_ins?}.count
         @longest_chip_ins_length = up_and_downs_scorecards.select{|scorecard| scorecard.chip_ins?}.map{|scorecard| scorecard.strokes.last(2).first.distance_from_hole}.max
-        @gir_to_within_5 = gir_scorecards.select{|scorecard| scorecard.strokes.putt.any? and scorecard.strokes.putt.first.distance_from_hole <= 5}.count
+        gir_to_within_5_scorecards = gir_scorecards.select{|scorecard| scorecard.strokes.putt.any? and scorecard.strokes.putt.first.distance_from_hole <= 5}
+        @gir_to_within_5 = gir_to_within_5_scorecards.count
         @gir_to_within_5_percentage = "#{gir_scorecards.count.zero? ? 0 : ((@gir_to_within_5.to_f / gir_scorecards.count) * 100).round(2)}%"
-        @holes_of_gir_to_within_5 = gir_scorecards.select{|scorecard| scorecard.strokes.putt.first.distance_from_hole <= 5}.map(&:number)
+        @holes_of_gir_to_within_5 = gir_to_within_5_scorecards.map(&:number)
         good_drives_scorecards = par_4_and_5_scorecards.select do |scorecard|
           [:left_rough, :right_rought, :unplayable].include?(scorecard.strokes.first.point_of_fall) and
           (scorecard.score - scorecard.putts - scorecard.penalties) <= (scorecard.par - 2)
