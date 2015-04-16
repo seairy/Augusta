c40cb989dd3c6adbdc16e69307a29179929ee3d6
seairy <seairy@gmail.com>
2015-04-16 18:38:26 +0800

    init commit

--- app/apis/v1/statistics_api.rb
+++ app/apis/v1/statistics_api.rb
@@ -219,7 +219,7 @@ module V1
           finished_matches_count: players.count,
           total_matches_count: @current_user.players.count
         }.merge(Hash[[:double_eagle, :eagle, :birdie, :par, :bogey, :double_bogey].map do |name|
-          ["#{name}_percentage", "#{scorecards.count.zero? ? 0 : ((scorecards.select{|scorecard| scorecard.send("#{name}?")}.count.to_f / scorecards.count) * 100).round}%",]
+          ["#{name}", scorecards.count.zero? ? 0 : (scorecards.select{|scorecard| scorecard.send("#{name}?")}.count.to_f / scorecards.count).round(2),]
         end])
         present entity
       end
