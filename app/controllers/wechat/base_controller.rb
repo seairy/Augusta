fae82683ef9c8fdaec1235125909a66e47eb3ba1
seairy <seairy@gmail.com>
2015-06-30 19:09:48 +0800

    init commit

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -4,17 +4,9 @@ class Wechat::BaseController < ApplicationController
   layout false
 
   def verify
-    if params[:signature] and params[:echostr] and params[:timestamp] and params[:nonce] and Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join) == params[:signature]
-      puts "*** return: #{params[:echostr]}"
+    if params[:signature] and params[:timestamp] and params[:nonce] and Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join) == params[:signature]
       render text: params[:echostr]
     else
-      puts "******* params[:signature]: #{params[:signature]}"
-      puts "******* params[:echostr]: #{params[:echostr]}"
-      puts "******* params[:timestamp]: #{params[:timestamp]}"
-      puts "******* params[:nonce]: #{params[:nonce]}"
-      puts "******* params.sort.join: #{[params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join}"
-      puts "******* sha1: #{Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join)}"
-      puts "*** return: failure"
       render text: 'failure'
     end
   end
