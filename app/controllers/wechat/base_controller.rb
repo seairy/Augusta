332b42483d7ab4159a2014c6c32ba5306eb6ac28
seairy <seairy@gmail.com>
2015-06-30 20:31:11 +0800

    init commit

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -4,7 +4,8 @@ class Wechat::BaseController < ApplicationController
   layout false
 
   def verify
-    puts "********* #{request.methods}"
+    puts "********* #{request.raw_post}"
+    puts "********* #{request.body}"
     if params[:signature] and params[:timestamp] and params[:nonce] and Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join) == params[:signature]
       render text: params[:echostr]
     else
