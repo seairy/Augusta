c55da135799175b471debb34b26f80a13a94732c
seairy <seairy@gmail.com>
2015-06-30 20:43:15 +0800

    init commit

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -6,7 +6,7 @@ class Wechat::BaseController < ApplicationController
   def verify
     if params[:signature] and params[:timestamp] and params[:nonce] and Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join) == params[:signature]
       if request.post?
-        hash = MultiXml.parse(request.raw_post)
+        hash = MultiXml.parse(request.raw_post)['xml']
         puts "****** hash: [#{hash}]"
       end
       render text: params[:echostr]
