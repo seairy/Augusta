2ad643390854ca946096067e65be05a637a69c34
seairy <seairy@gmail.com>
2015-07-02 18:47:56 +0800

    init commit

--- app/controllers/wechat/base_controller.rb
+++ app/controllers/wechat/base_controller.rb
@@ -6,8 +6,27 @@ class Wechat::BaseController < ApplicationController
   def verify
     if params[:signature] and params[:timestamp] and params[:nonce] and Digest::SHA1.hexdigest([params[:timestamp], params[:nonce], Setting.key[:wechat][:token]].sort.join) == params[:signature]
       if request.post?
-        hash = MultiXml.parse(request.raw_post)['xml']
-        puts "****** hash: [#{hash}]"
+        notification = MultiXml.parse(request.raw_post)['xml']
+        case notification['MsgType']
+        when 'text'
+          
+        when 'image'
+          
+        when 'location'
+          
+        when 'link'
+          
+        when 'event'
+          puts "****** #{notification}"
+        when 'voice'
+          
+        when 'video'
+          
+        when 'shortvideo'
+          
+        else
+          raise ArgumentError, 'Unknown Weixin Message'
+        end
       end
       render text: params[:echostr]
     else
