97eae45818cf551b2a3f757b0ea973ed7b46535a
seairy <seairy@gmail.com>
2015-07-14 10:57:08 +0800

    init commit

--- app/controllers/caddie/sessions_controller.rb
+++ app/controllers/caddie/sessions_controller.rb
@@ -3,7 +3,7 @@ class Caddie::SessionsController < Caddie::BaseController
   skip_before_action :authenticate
 
   def oauth2
-    redirect_to "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{Setting.key[:wechat][:appid]}&redirect_uri=http%3A%2F%2Filovegolfclub.com%2Fwechat%2Fcaddie%2Fsignin_with_open_id&response_type=code&scope=snsapi_base&state=caddiesignin#wechat_redirect"
+    redirect_to "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{Setting.key[:wechat][:appid]}&redirect_uri=http%3A%2F%2Filovegolfclub.com%2Fcaddie%2Fsignin_with_open_id&response_type=code&scope=snsapi_base&state=caddiesignin#wechat_redirect"
   end
 
   def create_with_open_id
