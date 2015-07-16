04a811862dbd6a133ceada4387595bdfa4f45b4d
seairy <seairy@gmail.com>
2015-07-16 11:04:27 +0800

    init commit

--- app/controllers/caddie/players_controller.rb
+++ app/controllers/caddie/players_controller.rb
@@ -3,4 +3,8 @@ class Caddie::PlayersController < Caddie::BaseController
   def index
     @players = @current_caddie.players.latest
   end
+
+  def show
+    @player = @current_caddie.players.find(params[:id])
+  end
 end
