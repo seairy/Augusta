f40e0aa03d7355dd74ebd565d97794fd176e13a3
seairy <seairy@gmail.com>
2015-07-16 16:03:24 +0800

    init commit

--- app/controllers/caddie/scorecards_controller.rb
+++ app/controllers/caddie/scorecards_controller.rb
@@ -6,14 +6,14 @@ class Caddie::ScorecardsController < Caddie::BaseController
 
   def update
     @scorecard = Scorecard.find(params[:id])
-    if @scorecard.caddie_id == @current_caddie.id
+    if @scorecard.player.caddie_id == @current_caddie.id
       @scorecard.update!(scorecard_params)
     end
     redirect_to [:caddie, @scorecard.player]
   end
 
   protected
-  def scorecard_params
-    params.require(:scorecard).permit!
-  end
+    def scorecard_params
+      params.require(:scorecard).permit!
+    end
 end
