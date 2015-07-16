8d75c7df85c1ccfae33f80a165f9d2c960c28481
seairy <seairy@gmail.com>
2015-07-16 14:34:01 +0800

    init commit

---
+++ app/controllers/caddie/scorecards_controller.rb
@@ -0,0 +1,15 @@
+# -*- encoding : utf-8 -*-
+class Caddie::ScorecardsController < Caddie::BaseController
+  def edit
+    @scorecard = Scorecard.find(params[:id])
+    if @scorecard.caddie_id == @current_caddie.id
+      @scorecard.update!(scorecard_params)
+    end
+    redirect_to [:caddie, @scorecard.player]
+  end
+
+  protected
+  def scorecard_params
+    params.require(:scorecard).permit!
+  end
+end
